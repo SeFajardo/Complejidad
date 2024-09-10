3da3996fc45ae93a370bf501193b95077722f9e4
Sebastián Fajardo García <s.fajardog@uniandes.edu.co>
2024-09-10 23:40:06 +0000

    Codigos 2 y 3

---
+++ codigo2.py
@@ -0,0 +1,18 @@
+from time import time
+from memory_profiler import profile
+@profile
+def prueba():
+    sum = 0
+    n = int(input("Número: "))
+    i = 1
+    start_time = time()
+    while (i < n):
+        i = i*2
+        j = 0
+        while (j < i):
+            sum = sum + 1
+            j = j +1
+    print(sum)
+    elapsed_time = time() - start_time
+    print("Tiempo trascurrido: %.20f seconds." % elapsed_time)
+prueba()
